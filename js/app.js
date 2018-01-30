3c3e3c28530fabfa7c7555414509147e266effcc
TaniaNaOH <tania.naoh83@gmail.com>
2018-01-29 20:19:52 -0600

    cambios hasta las 8:20pm

--- js/app.js
+++ js/app.js
@@ -1,8 +1,10 @@
 
  $(document).ready(function(){
+     $('.parallax').parallax();
      $('ul.tabs').tabs('select_tab', 'tab_id');
    });
 
    $(document).ready(function(){
-        $('.parallax').parallax();
-      });
+      // the "href" attribute of the modal trigger must specify the modal ID that wants to be triggered
+      $('.modal').modal();
+    });
