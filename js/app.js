1bda7e93c1029a567efcc3924be39e58ea4aa588
aciremaaleman <aciremaaleman@outlook.es>
2018-01-29 18:23:10 -0600

    Actualizando cambios de html, js y css

--- js/app.js
+++ js/app.js
@@ -1,3 +1,34 @@
+
+var $name = $('#name');
+var $password = $('#password');
+
 $(document).ready(function(){
-    $('ul.tabs').tabs('select_tab', 'tab_id');
-  });
+  $('ul.tabs').tabs('select_tab', 'tab_id');
+  $('.parallax').parallax();
+  $name.keyup(validate);
+  $password.keyup(validate);
+});
+
+var validate = function(){
+  if ($(this).val().trim().length>0){
+    $('#enter').removeAttr('disabled');
+  } else {
+    $('#enter').attr('disabled', true);
+  };
+};
+
+
+
+
+
+
+  // Initialize Firebase
+  // var config = {
+  //   apiKey: "AIzaSyAlAZ7lD_ek20WCGhSNS73SvALNyHrQYU0",
+  //   authDomain: "foodlovers-a505a.firebaseapp.com",
+  //   databaseURL: "https://foodlovers-a505a.firebaseio.com",
+  //   projectId: "foodlovers-a505a",
+  //   storageBucket: "",
+  //   messagingSenderId: "703106499379"
+  // };
+  // firebase.initializeApp(config);
